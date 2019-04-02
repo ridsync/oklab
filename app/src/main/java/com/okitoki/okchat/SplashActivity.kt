e6977559a23996b161148c1b5f2873f7293dc2b8
ridsync <ridsync@naver.com>
2019-04-02 22:56:48 +0900

    프로젝트 기본 세팅

---
+++ app/src/main/java/com/okitoki/okchat/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.okitoki.okchat
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by okc on 2019-04-01.
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+
+}
