727d53705ed11f2c54947f80eec9ceb40ae31fff
ridsync <ridsync@naver.com>
2020-12-23 23:47:37 +0900

    [OKLab-Android] AWS S3 파일 업로드( Amplify / AWSMobileClient ) 테스트 1차 완료.

---
+++ amplify/backend/function/S3Trigger3eb79750/src/index.js
@@ -0,0 +1,7 @@
+exports.handler = function (event, context) {
+  console.log('Received S3 event:', JSON.stringify(event, null, 2));
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+  console.log(`Bucket: ${bucket}`, `Key: ${key}`);
+  context.done(null, 'Successfully processed S3 event');
+};
