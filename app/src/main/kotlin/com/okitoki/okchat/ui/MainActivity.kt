af4403dca567c30b8cbf6d93f17c138c08556ecf
DevOKs <ridsync@gmail.com>
2021-01-26 15:03:27 +0900

    다이얼로그 프래그먼트 추가

--- app/src/main/kotlin/com/okitoki/okchat/ui/MainActivity.kt
+++ app/src/main/kotlin/com/okitoki/okchat/ui/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.okitoki.okchat.ui
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.okitoki.okchat.R
+import com.okitoki.okchat.ui.dialog.TestDialogFragment
 import com.okitoki.okchat.ui.testui.TestLabMainFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG_DIALOG_EVENT = "TAG_DIALOG_EVENT"
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -37,5 +43,18 @@ class MainActivity : AppCompatActivity() {
         frTransaction.commit()
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        findViewById<Button>(R.id.mrbButton).setOnClickListener {
+//            val bundle = Bundle()
+//            bundle.putParcelable(EXTRA_NOTICE_SAVE, FavoriteNotice(it.num, it.title, it.link))
+            val dialog: TestDialogFragment = TestDialogFragment.newInstance()
+//            dialog.arguments = bundle
+            supportFragmentManager.let { fragmentManager ->
+                dialog.show(
+                    fragmentManager,
+                    TAG_DIALOG_EVENT
+                )
+            }
+        }
     }
 }
---
+++ app/src/main/kotlin/com/okitoki/okchat/ui/dialog/TestDialogFragment.kt
@@ -0,0 +1,35 @@
+package com.okitoki.okchat.ui.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.okitoki.okchat.R
+
+/**
+ * Created by okwon on 2021/01/12.
+ */
+class TestDialogFragment : DialogFragment() , View.OnClickListener{
+
+    companion object {
+        fun newInstance(): TestDialogFragment {
+            return TestDialogFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.dialog_test_fragment, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+
+    override fun onClick(p0: View?) {
+        dismiss()
+    }
+
+}
